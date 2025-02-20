98f570a226646c412c27887cac384c8db0be8ea1
unknown <dlrytnwkd@naver.com>
2025-02-20 23:56:16 +0900

    user service update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
@@ -6,7 +7,21 @@ import { UsersModule } from './users/users.module';
 import { UserMovieReviewsModule } from './user_movie_reviews/user_movie_reviews.module';
 
 @Module({
-  imports: [MoviesModule, UsersModule, UserMovieReviewsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres', // or your preferred database
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.NODE_ENV !== 'production',
+    }),
+    MoviesModule,
+    UsersModule,
+    UserMovieReviewsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @MinLength(4)
+  username: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+}
